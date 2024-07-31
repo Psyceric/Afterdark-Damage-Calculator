4876d138439b0ab80d307b9a8ccbb961f107b86b
Jason clini <33248688+Psyceric@users.noreply.github.com>
2024-07-31 19:05:35 -0400

    Calculating Tag Damage

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,5 +1,7 @@
 import csv, os, re
 
+weapon_list = []
+
 def main():
     path = os.getcwd()
     parentDirectory = os.path.abspath(os.path.join(path,os.pardir))
@@ -7,25 +9,36 @@ def main():
     fileName = './resources/Afterdark 1.02 Weapon Values.csv'
     resourcePath = os.path.abspath(os.path.join(parentDirectory,fileName))
     print(resourcePath)
-
-    fields = []
-    rows = []
-
+    
     with open(resourcePath, 'r') as csvfile:
         csvreader = csv.DictReader(csvfile)
-        weapon_list = []
-
         for row in csvreader:
             weapon_list.append(row)
-            if parseDamageDice(row['Damage Roll']): print(row['Weapon Name'], "| Damage Validated :", row['Damage Roll'])
+            parseWeapon(row)
+            #Automatic Weapons .5 Tag Damage
+            #Flexible,Cleaving,Piercing,Incindiary,Explosive 1 Tag Damage
 
-
-def parseDamageDice(Dice):
+def parseWeapon(Weapon):
     dicePattern = "^\\d{1,3}[d]([4,6,8]|]|1[0,2])$" #Regex that Validates number of dice of dice types {4,6,8,10,12}
-    if(re.fullmatch(dicePattern,Dice)) : 
-        return True
-    else:
-        return False
+
+    if not (re.fullmatch(dicePattern,Weapon['Damage Roll'])) : 
+        raise TypeError("Unable to Validate Dice", Weapon['Weapon Name'])
+    keywords = Weapon['Weapon Tags'].split(',')
+    tagDamage = 0.0
+    for keyword in keywords:
+        _keyword = keyword.strip()
+        match _keyword:
+            case "Automatic":
+                tagDamage += .5
+            case "Flexible"|"Cleaving"|"Piercing"|"Incindiary"|"Explosive":
+                tagDamage += 1
+    print("{0}{1} | {2} : {3}".format(
+        Weapon['Weapon Name'],                      #{0}
+        " " * (25 - len(Weapon['Weapon Name'])),    #{1}
+        Weapon['Damage Roll'],                      #{2}
+        tagDamage))                                 #{3}
+    
+    return True
 
 if __name__ == "__main__":
     main()
