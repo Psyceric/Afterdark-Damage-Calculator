389347442af5615c00882d85c73991a43fb32235
Jason clini <33248688+Psyceric@users.noreply.github.com>
2024-07-31 22:22:33 -0400

    Weapon Leveling Method

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,10 +1,11 @@
-import csv, os, re
+import csv, os, re, math
 
 weapon_list = []
 fileName = './resources/Afterdark 1.02 Weapon Values.csv'
 
 def main():
     initilizeWeaponList()
+
 
 def initilizeWeaponList():
     path = os.getcwd()
@@ -17,7 +18,8 @@ def initilizeWeaponList():
         for row in csvreader:
             weapon_list.append(row)
             parse_weapon(row)
-            print(row,"\n")
+            level_weapon(row,4)     #- For testing immedietly level to 4
+            print(row,"\n")         #- GET DICT TO PRINT
 
 def parse_weapon(Weapon):
     verify_weapon(Weapon)
@@ -39,35 +41,42 @@ def parse_weapon(Weapon):
             case "Wieldy":
                 toHitBonus += 1
     
-        #print("{0}{1} | {2} : {3} |\tBase Damage: {4}".format(
-        #Weapon['Weapon Name'],                                 #{0}
-        #" " * (25 - len(Weapon['Weapon Name'])),               #{1}
-        #Weapon['Damage Roll'],                                 #{2}
-        #tagDamage,                                             #{3}
-        #baseDamage                                             #{4}
+    # print("{0}{1} | {2} : {3} |\tBase Damage: {4}".format(   #Prints Default Weapon Stats
+    # Weapon['Weapon Name'],                                   #{0}                       |
+    # " " * (25 - len(Weapon['Weapon Name'])),                 #{1}       Does Not        |
+    # Weapon['Default Damage'],                                #{2}  Include Range/Ammo   |
+    # tagDamage,                                               #{3}                       |
+    # baseDamage))                                             #{4}______________________/
                                                      
-    Weapon['Base Damage'] = baseDamage
-    Weapon['Level'] = 1
-    Weapon['Damage Dice'] = get_damage_dice(Weapon['Damage Roll'])
-    Weapon['Damage Modifier'] = damageModifier
-    Weapon['To Hit Bonus'] = toHitBonus
-    return True
+    Weapon['_baseDamage'] = baseDamage
+    Weapon['_level'] = 1
+    Weapon['_damageDice'] = get_damage_dice(Weapon['Default Damage'])
+    Weapon['_damageModifier'] = damageModifier
+    Weapon['_toHitBonus'] = toHitBonus
 
 def get_base_damage(Weapon):
-    dmgDice = get_damage_dice(Weapon["Damage Roll"])
+    dmgDice = get_damage_dice(Weapon['Default Damage'])
     dmgCalc = (int(dmgDice[0]) * (int(dmgDice[1])+1) / 2)
     return (str(dmgCalc).zfill(4))
+
+def level_weapon(Weapon, level):
+    defaultDamage = get_damage_dice(Weapon['Default Damage'])
+    Weapon['_level'] = level
+
+    diceFaceMod = min(math.floor(level/2),2)
+    diceFace = int(defaultDamage[1]) + diceFaceMod*2
+    diceQuantityMod = level-diceFaceMod
+    diceQuantity = int(defaultDamage[0]) + diceQuantityMod
+    Weapon['Damage Dice'] = [diceQuantity,diceFace]
 
 def get_damage_dice(Dice): 
     return Dice.split('d')
 
-def level_weapon(Weapon, level):
-    Weapon["Damage Roll"]
-
 def verify_weapon(Weapon):
     dicePattern = "^\\d{1,3}[d]([4,6,8]|]|1[0,2])$" #Regex that Validates number of dice of dice types {4,6,8,10,12}
-    if not (re.fullmatch(dicePattern,Weapon['Damage Roll'])) : 
-        raise TypeError("Unable to Validate Dice", Weapon['Weapon Name'])
+    if not (re.fullmatch(dicePattern,Weapon['Default Damage'])) : 
+        raise TypeError("Unable to Validate Default Damage for '", Weapon['Weapon Name'],"'")
 
 if __name__ == "__main__":
     main()
+
